c8df9ffb9f9ebb84e2516b6eac683166a531277c
Maria Ragone <maria@ragone.net>
2023-07-27 11:08:38 -0400

    Pseudocode created, added loop but not done

--- Library Project - Group 3/Program.cs
+++ Library Project - Group 3/Program.cs
@@ -43,32 +43,23 @@ List<Book> books = new List<Book>()
 {
     new Book("The Great Gatsby", "F. Scott Fitzgerald", "on shelf"),
     new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling","checked out", DueDate),
-    new Book("To Kill a Mockingbird","Harper Lee", "on shelf"),    
+    new Book("To Kill a Mockingbird","Harper Lee", "on shelf"),
     new Book("The Hobbit", "J.R.R. Tolkein", "checked out", DueDate),
     new Book("1984", "George Orwell", "on shelf"),
     new Book("Siddhartha", "Herman Hesse", "on shelf"),
     new Book("For Whom the Bell Tolls", "Ernest Hemingway", "checked out", DueDate),
     new Book("The Stand", "Stephen King", "on shelf"),
-    new Book("The Hobbit", "J.R.R. Tolkein", "on shelf"),
+    new Book("The Lord of the Rings", "J.R.R. Tolkein", "on shelf"),
     new Book("The Old Man and the Sea", "Ernest Hemingway", "checked out", DueDate),
-    new Book("The Shining", "Stephen King", "on shelf"),    
+    new Book("The Shining", "Stephen King", "on shelf"),
     new Book("Bossypants", "Tina Fey", "checked out", DueDate),
     new Book("Me Talk Pretty One Day", "David Sedaris", "on shelf"),
     new Book("Where'd You Go, Bernadette", "Maria Semple", "on shelf"),
     new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", "on shelf")
 
 };
-
-
-DisplayMenu(books);
-
-//for (int i = 0; i < books.Count; i++)
-//{
-//    Console.WriteLine($"{i + 1}. {books[i]}");
-//}
-
-
-
+//-----------------------------------------------------------------------------------
+//puts the list above onto the file
 string Status = "";
 StreamWriter writer = new StreamWriter(filePath);
 foreach (Book bk in books)
@@ -85,34 +76,145 @@ foreach (Book bk in books)
     }
 }
 writer.Close();
+///---------------------------------------------------------------------------------
+
+
+////PSEUDOCODE
+//start program and display menu options
+//Chose to display all books or search, or retrun book
+    //if display all books as numbered list
+        //show the entire list of books
+        //search by author, title, keyword
+    //if search
+        //prompt the user to search by author, title, or keyword
+        //display the searchlist that matches search term - list is numbered
+        //at the end of the list there is extra numbers for checkout, search again, see entire list, quit
+            //if checkout
+                //change status and due date for selected book
+            //if search again
+                //loop the "search" if statement above
+            //if display entire list
+                //search by author, title, keyword
+    //if return book
+        //show the list of books that are checked out (status = checked out)
+        //which book do you want to return?
+        //change status and remove due date for selected book
+    //if quit
+        //quit the program
+                
+
+
+
+//DISPLAY ALL BOOKS
+DisplayMenu(books);
+
+
+
+
 
 //revisit whether to search seperately or not allowing character 
-Console.WriteLine("Search by Author,Title or keyword");
-string choice = Console.ReadLine().ToLower().Trim();
+bool runProgram = true;
 
-if(books.Any(b => b.Title.ToLower().Trim().Contains(choice)))
+while (runProgram)
 {
-    List<Book> searchList = books.Where(b => b.Title.ToLower().Trim().Contains(choice)).ToList();
+    Console.WriteLine("Search by Author,Title or keyword");
+    string choice = Console.ReadLine().ToLower().Trim();
 
-    foreach (Book bk in searchList)
+
+    if (books.Any(b => b.Title.ToLower().Trim().Contains(choice)))
     {
-        Console.WriteLine(bk);
+        List<Book> searchList = books.Where(b => b.Title.ToLower().Trim().Contains(choice)).ToList();
+
+        DisplayMenu(searchList);
+        break;
     }
-}
 
-else if (books.Any(b => b.Author.ToLower().Trim().Contains(choice)))
-{
-    List<Book> searchList = books.Where(b => b.Author.ToLower().Trim().Contains(choice)).ToList();
-
-    foreach (Book bk in searchList)
+    else if (books.Any(b => b.Author.ToLower().Trim().Contains(choice)))
     {
-        Console.WriteLine(bk);
+        List<Book> searchList = books.Where(b => b.Author.ToLower().Trim().Contains(choice)).ToList();
+
+        DisplayMenu(searchList);
+        break;
     }
-}
-else
-{
+    else
+    {
+        Console.WriteLine("Not found.");
+
+    }
+
+    Console.WriteLine("");
+    string checkOut = Console.ReadLine().Trim().ToLower();
+
+
+
+
+
+    //looping
+ 
+
+    //    int menuChoice = -1;
+    //while (menuChoice <= -1 || menuChoice >= Allcar.Count + 3)
+    //{
+    //    Console.WriteLine($"Choose a menu option. 1-{Allcar.Count + 2}");
+    //    while (int.TryParse(Console.ReadLine(), out menuChoice) == false)
+    //    {
+    //        Console.WriteLine("incorrect format");
+    //    }
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    menuChoice
+
+
+
+
+    //    //LOGIC for buying Car
+    //    if (checkOut == "y")
+    //    {
+    //        books.Remove(choice);
+    //        Console.WriteLine("Our finance team will reach out to you shortly.");
+    //    }
+
+    //    else
+    //    {
+    //        Console.WriteLine("Feel free to keep browsing.");
+
+    //    }
+    //    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -124,9 +226,12 @@ static void DisplayMenu(List<Book> bookList)
 {
     for (int i = 0; i < bookList.Count; i++)
     {
-        Console.WriteLine($"{i+1}. {bookList[i]}");
-        
+        Console.WriteLine($"{i + 1}. {bookList[i]}");
+
     }
+    Console.WriteLine($"{bookList.Count + 1} Check out");
+    Console.WriteLine($"{bookList.Count + 2} Search again");
+    Console.WriteLine($"{bookList.Count + 3} Quit");
 }
 
 //bool runProgram = true;
